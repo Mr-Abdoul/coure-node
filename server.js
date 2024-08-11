35df3b81d104688683b220b0ce84dba45c7a20dc
Le-Ministre <abdourahamaneabdoulaye36@gmail.com>
2024-08-11 14:30:43 +0100

    create express server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const fs = require('fs');
+// const _ = require('loadash');
 
 const server = http.createServer((req, res) =>{
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    res.setHeader('content-type', 'text/plain')
+    res.write('hello, sani')
+    res.end();
+
 });
+
 
 server.listen(3000, 'localhost', () =>{
     console.log('listen for request on port 3000');
