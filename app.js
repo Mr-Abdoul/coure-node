b8f40003a51b5a4d69755da98788c99a522fecaf
Le-Ministre <abdourahamaneabdoulaye36@gmail.com>
2024-08-13 19:52:30 +0100

    creat partials

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) =>{
-    res.sendFile('./views/index.html', {root: __dirname})
+   
+    res.render('index', {title: 'Home'});
+})
+app.get('/about', (req, res) =>{
+    res.render('about', {title: 'About'});
+})
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'Creat a new Blog'});
+})
+
+app .use((req,res) =>{
+    res.status(404).render('404', {title: '404'});
 })
